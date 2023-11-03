595b0ad819c3a4b3d689103f31109e2de181498e
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-11-04 00:28:26 +0600

    full done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,49 @@
 import React from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <h1>this is login page (not done)</h1>
-    </div>
+    <Container>
+      <Form className="mx-auto w-50 mt-5">
+        <h3 className="mb-4">Please Login</h3>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            required
+            placeholder="Enter email"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+          <Form.Text className="text-danger">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+
+        <Form.Group>
+          <Form.Text>
+            Don't Have an Account? <Link to="/register">Please Register</Link>
+          </Form.Text>
+        </Form.Group>
+      </Form>
+    </Container>
   );
 };
 
--- src/Pages/Login/Register.jsx
+++ src/Pages/Login/Register.jsx
@@ -1,10 +1,69 @@
 import React from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>
-      <h3>This is Register page</h3>
-    </div>
+    <Container>
+      <Form className="mx-auto w-50 mt-5">
+        <h3 className="mb-4">Please Register</h3>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            required
+            placeholder="Enter you name"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Photo URL</Form.Label>
+          <Form.Control
+            type="text"
+            name="photo"
+            required
+            placeholder="Your photo url"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            required
+            placeholder="Enter email"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check
+            type="checkbox"
+            name="accept"
+            label="Accept the terms and conditions"
+          />
+          <Form.Text className="text-danger"></Form.Text>
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Register
+        </Button>
+
+        <Form.Group>
+          <Form.Text>
+            Already Have an Account? <Link to="/login">Please login</Link>
+          </Form.Text>
+        </Form.Group>
+      </Form>
+    </Container>
   );
 };
 
